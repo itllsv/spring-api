b99b8e68e4dacafc0688dae80e179a1968d63eef
itllsv <itallo.sa.vieira@gmail.com>
2026-03-16 16:11:32 -0300

    feat: add repositories

---
+++ src/main/java/com/eventsss/api/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.eventsss.api.repositories;
+
+import com.eventsss.api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+}
